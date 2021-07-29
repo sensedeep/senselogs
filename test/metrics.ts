d37776ecbbf1b9f79dfa0a2efb1335502f0888fe
Michael O'Brien <mob-pub-18@emobrien.com>
2021-07-29 10:02:28 +1000

    TEST: fix metrics test

--- test/metrics.ts
+++ test/metrics.ts
@@ -10,7 +10,7 @@ test('metrics', async() => {
 
     log.metrics('Acme/Rockets', {sessions: 1})
     let result: any = log.flush()[0]
-    expect(result.indexOf('{"_aws":')).toBe(0)
+    expect(result.indexOf('{"_aws":') >= 0).toBe(true)
     expect(result.indexOf('"Namespace":"Acme/Rockets"') > 0).toBe(true)
     expect(result.indexOf('"Name":"sessions"') > 0).toBe(true)
 })
