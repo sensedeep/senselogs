c0165d542d6f3f2081813b96beaf47d56f2b7fa7
Michael O'Brien <mob-pub-18@emobrien.com>
2021-07-29 10:02:05 +1000

    DEV: improve TS signature for setDestination

--- src/index.d.ts
+++ src/index.d.ts
@@ -19,19 +19,18 @@ export default class SenseLogs {
     context: {};
     constructor(options?: ConstructorOptions, context?: {});
     addContext(contexts: {} | {}[]): SenseLogs;
-    addDestination(dest: function): SenseLogs;
+    addDestination(dest: Destination): SenseLogs;
     addFilter(filter?: string | string[]): SenseLogs;
     addLevels(levels?: string | string[]): SenseLogs;
     child(context?: {}): SenseLogs;
     clearContext(): SenseLogs;
-    emit(level: string, message: string, context?: {}): void;
     flush(): {}[];
     getFilter(): string[];
     getLevels(): string[];
     getSample(): {};
     getOverride(): {};
     metrics(namespace: string, values: {}, dimensions?: any[][]): void;
-    setDestination(dest: function): SenseLogs;
+    setDestination(dest: Destination): SenseLogs;
     setFilter(filter?: string | string[]): SenseLogs;
     setLevels(levels?: string | string[]): SenseLogs;
     setOverride(filter?: string | string[], expire?: Date): SenseLogs;
@@ -45,4 +44,7 @@ export default class SenseLogs {
     info(message: string | Error, context?: {}): void;
     trace(message: string | Error, context?: {}): void;
     warn(message: string | Error, context?: {}): void;
-};
+
+    //  Custom levels
+    emit(level: string, message: string, context?: {}): void;
+}
